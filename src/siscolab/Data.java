d096e5845b345a77233a3b8f4333ac7313252383
U-UPD1\20171BSI0278 <20171BSI0278@SR6736.cefetes.br>
2019-03-29 11:29:54 -0300

    Criando classes Exames e Data

---
+++ src/siscolab/Data.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package siscolab;
+
+/**
+ *
+ * @author 20171BSI0278
+ */
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+    
+    public void data(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
