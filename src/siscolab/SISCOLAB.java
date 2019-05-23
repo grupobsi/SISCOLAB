90878283a1c45f3858e3660c31c13ca2856a5eb1
uberraschung <matheus_garcias@hotmail.com>
2019-05-23 00:14:42 -0300

    Adicionando código refatorado esquecido

--- src/siscolab/SISCOLAB.java
+++ src/siscolab/SISCOLAB.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package siscolab;
+import siscolab.modelos.PersistenciaArquivo;
+import siscolab.modelos.Medico;
+import siscolab.modelos.Especialidade;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
